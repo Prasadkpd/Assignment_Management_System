43b89267538d7b8cbfd47e0a204edc06aa0151bb
Prasadkpd <2019cs078@stu.ucsc.lk>
2022-06-30 09:16:45 +0530

    Profile.js done

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,9 +1,51 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import jwt_decode from 'jwt-decode';
 
 const Profile = () => {
-    return (
-        <div>
 
+    const initialState = {
+        first_name: '',
+        last_name: '',
+        email: '',
+        password: ''
+    }
+
+    useEffect(()=>{
+       const token = localStorage.usertoken
+        const decoded = jwt_decode(token);
+       setProfileState({
+           first_name: decoded.first_name,
+           last_name: decoded.last_name,
+           email: decoded.email
+       })
+    },[]);
+
+    const [profileState, setProfileState] = useState(initialState);
+    return (
+        <div className='container'>
+            <div className='jumbotron mt-5'>
+                <div className='col-sm8 mx-auto'>
+                    <h1 className="text-center">Profile</h1>
+                </div>
+                <table className="table col-md-6 mx-auto">
+                    <h2>
+                       <tbody>
+                       <tr>
+                           <td>First Name</td>
+                           <td>{profileState.first_name}</td>
+                       </tr>
+                       <tr>
+                           <td>Last Name</td>
+                           <td>{profileState.last_name}</td>
+                       </tr>
+                       <tr>
+                           <td>Email</td>
+                           <td>{profileState.email}</td>
+                       </tr>
+                       </tbody>
+                    </h2>
+                </table>
+            </div>
         </div>
     );
 };
